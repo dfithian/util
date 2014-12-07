4e05e1b7b98031bef3b421fea1747ddeb02946d9
Daniel Fithian <daniel.m.fithian@gmail.com>
2014-12-07 01:23:37 -0500

    convert tabs to spaces

--- test/src/TestConfig.cs
+++ test/src/TestConfig.cs
@@ -4,29 +4,29 @@ using Newtonsoft.Json;
 
 namespace Test
 {
-	public class TestConfig
-	{
-		public static String filename = "test.cfg";
-		[JsonPropertyAttribute("stringField")]
-		public String stringField;
-		[JsonPropertyAttribute("intField")]
-		public int intField;
-		[JsonPropertyAttribute("boolField")]
-		public bool boolField;
-		[JsonPropertyAttribute("arrayField")]
-		public String[] arrayField;
+    public class TestConfig
+    {
+        public static String filename = "test.cfg";
+        [JsonPropertyAttribute("stringField")]
+        public String stringField;
+        [JsonPropertyAttribute("intField")]
+        public int intField;
+        [JsonPropertyAttribute("boolField")]
+        public bool boolField;
+        [JsonPropertyAttribute("arrayField")]
+        public String[] arrayField;
 
-		public void Debug() {
-			Logger logger = LoggerFactory.GetLogger(typeof(TestConfig));
-			logger.Debug("stringField: " + stringField);
-			logger.Debug("intField: " + intField.ToString());
-			logger.Debug("boolField: " + boolField.ToString());
-			logger.Debug("arrayField...");
-			if (arrayField != null) {
-				foreach (String s in arrayField) {
-					logger.Debug("array string: " + s);
-				}
-			}
-		}
-	}
+        public void Debug() {
+            Logger logger = LoggerFactory.GetLogger(typeof(TestConfig));
+            logger.Debug("stringField: " + stringField);
+            logger.Debug("intField: " + intField.ToString());
+            logger.Debug("boolField: " + boolField.ToString());
+            logger.Debug("arrayField...");
+            if (arrayField != null) {
+                foreach (String s in arrayField) {
+                    logger.Debug("array string: " + s);
+                }
+            }
+        }
+    }
 }
